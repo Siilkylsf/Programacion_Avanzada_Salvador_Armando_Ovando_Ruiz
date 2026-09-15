baac3a6b52ec627df5b320f9459652043121f68c
Salvador Armando Ovando Ruiz <66149809+Siilkylsf@users.noreply.github.com>
2026-09-15 10:37:34 -0600

    Add files via upload

---
+++ Ejercicio 3/Ejercicio 3/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("EJERCICIO_2");                                               //Numero de la practica
+Console.WriteLine("CÁLCULO DEL PROMEDIO DE TRES CALIFICACIONES");               //Nombre del ejercicio
+Console.WriteLine();                                                            //Instruccion para dejar un espacio en blanco en la consola
+Console.Write("Ingrese la primera calificación: ");
+double calificacion1 = Convert.ToDouble(Console.ReadLine());                //Declaramos la variable 1
+Console.Write("Ingrese la segunda calificación: ");
+double calificacion2 = Convert.ToDouble(Console.ReadLine());                //Declaramos la variable 2
+Console.Write("Ingrese la tercera calificación: ");
+double calificacion3 = Convert.ToDouble(Console.ReadLine());                //Declaramos la variable 3
+double promedio = (calificacion1 + calificacion2 + calificacion3) / 3.0;    //Calculo del promedio de las calificaciones, redondeado a 2 decimales
+Console.WriteLine();
+Console.WriteLine($"El promedio es: {promedio:F2}");                            //Muestra el promedio en la consola
+Console.WriteLine();
+if (promedio >= 90)                                                                 //Condición para determinar si el promedio es aprobado o reprobado
+{
+    Console.WriteLine("Alumno excelente.");                        //Mensaje de aprobación
+}
+else if (promedio <= 89 && promedio >= 80)                                                                 //Condición para determinar si el promedio es aprobado o reprobado
+{
+    Console.WriteLine("Alumno bueno.");                        //Mensaje de aprobación
+}
+else if (promedio <= 79 && promedio >= 70)                                                                 //Condición para determinar si el promedio es aprobado o reprobado
+{
+    Console.WriteLine("Alumno regular.");                        //Mensaje de aprobación
+}
+else if (promedio <= 69 && promedio >= 51)                                                                 //Condición para determinar si el promedio es aprobado o reprobado
+{
+    Console.WriteLine("Alumno requiere apoyo.");                        //Mensaje de reprobación
+}
+else if (promedio <= 50)                                                                 //Condición para determinar si el promedio es aprobado o reprobado
+{
+    Console.WriteLine("Alumno deficiente.");                        //Mensaje de reprobación
+}
